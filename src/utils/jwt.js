32c40104f5d09f236889083c08f0a61a9b325b38
sinan <sinanbh9605@gmail.com>
2025-11-21 21:11:08 +0530

    fix:bug

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,16 +1,27 @@
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-const { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET } = process.env;
-
 export const generateAccessToken = async (userPayload) => {
-  return jwt.sign(userPayload, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+  if (!process.env.ACCESS_TOKEN_SECRET) {
+    throw new Error("ACCESS_TOKEN_SECRET is not defined");
+  }
+  return jwt.sign(userPayload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
 };
 export const generateRefreshToken = async (userPayload) => {
-  return jwt.sign(userPayload, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+  if (!process.env.REFRESH_TOKEN_SECRET) {
+    throw new Error("REFRESH_TOKEN_SECRET is not defined");
+  }
+  return jwt.sign(userPayload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
 };
 
 export const verifyRefreshToken = async (refreshToken) => {
-  return jwt.verify(refreshToken, REFRESH_TOKEN_SECRET) ;
+  if (!process.env.REFRESH_TOKEN_SECRET) {
+    throw new Error("REFRESH_TOKEN_SECRET is not defined");
+  }
+  return jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
 };
